82418a7d6ff44ba60c48a81ffb749a91457383e6
titianteiger <titians@gmx.ch>
2016-07-31 16:40:50 +0200

    Added a simple perceptron example to the repo.

---
+++ perceptron/iris_perceptron.py
@@ -0,0 +1,58 @@
+# iris_perceptron.py
+
+# dataset from https://archive.ics.uci.edu/ml/machine-learning-databases/iris/
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from perceptron import Perceptron
+
+data = pd.read_csv("iris.data", header=None)
+
+# set column headers
+data.columns = ["Sepal length", "Sepal width", "Petal length", "Petal width",
+                "Class label"]
+
+# only select the setosa and versicolor species
+data = data.loc[(data["Class label"] == "Iris-setosa") |
+                (data["Class label"] == "Iris-versicolor")]
+
+# convert species labels to integers, -1 for setosa and 1 for versicolor
+data["Class label"] = np.where(data["Class label"]=="Iris-setosa", -1, 1)
+
+# split data into feature matrix (only "Sepal length" and "Petal length" are
+# used) and target vector
+X = data[["Sepal length", "Petal length"]]
+y = data["Class label"]
+
+# plot the sepal lenght versus the petal length for both iris species
+fig1 = plt.figure()
+ax = fig1.add_subplot(1, 1, 1)
+
+# setosa iris
+ax.scatter(X["Sepal length"].iloc[:50], X["Petal length"].iloc[:50],
+           color="red", marker="o", label="Iris setosa")
+
+# versicolor iris
+ax.scatter(X["Sepal length"].iloc[50:], X["Petal length"].iloc[50:],
+           color="blue", marker="o", label="Iris versicolor")
+
+ax.set(xlabel="Sepal lenght", ylabel="Petal length")
+ax.legend(loc="upper left")
+
+# train the perceptron classifier
+iris_ppn = Perceptron()
+iris_ppn.fit(X.as_matrix(), y)
+
+# plot the numbers of errors for each perceptron iteration
+fig2 = plt.figure()
+ax = fig2.add_subplot(1, 1, 1)
+
+ax.plot(range(1, iris_ppn.n_iterations + 1), iris_ppn.errors_, marker="o")
+
+ax.set(xlabel="Iteration", ylabel="Number of missclassifications")
+
+
+
+plt.show()
+
---
+++ perceptron/perceptron.py
@@ -0,0 +1,71 @@
+# perceptron.py
+
+import numpy as np
+
+class Perceptron(object):
+    """Implementation of a perceptron classifier for two-class classification.
+
+    Arguments:
+    eta -- learning rate between 0 and 1
+    n_iterations -- number of iterations over the training dataset
+    """
+    def __init__(self, eta=0.01, n_iterations=10):
+        self.eta = eta
+        self.n_iterations = n_iterations
+
+    def fit(self, X, y):
+        """Fit the Perceptron object to the training data. Creates the
+        attributes w_, an array of the weights after the data fitting, and
+        errors_, which lists the number of misclassifications in each iteration.
+
+        Arguments:
+        X -- training data matrix, each row corresponds to a sample and each collumn
+             to a certain feature
+        y -- target vector, containing the target values for each sample
+
+        Returns:
+        self -- fitted Perceptron object with new attributes
+        """
+        # initialize the new attributes
+        self.w_ = np.zeros(1 + X.shape[1])  # create an extra slot for w0
+        self.errors_ = []
+
+        # loop through each iteration step
+        for _ in range(self.n_iterations):
+            errors = 0
+
+            for xi, yi in zip(X, y):
+                # update the weights using the predict method that calculates
+                # the output label of the percepton model
+                delta_w = self.eta * (yi - self.predict(xi))
+                self.w_[0] += delta_w * 1 # x0 = 1
+                self.w_[1:] += delta_w * xi
+
+                # increase the error count if delta_w changes which means that
+                # a prediction was wrong
+                errors += int(delta_w != 0)
+
+            self.errors_.append(errors)
+
+        return self
+                
+    def calculate_net_input(self, x):
+        """Calculate the value of the net input function for the given data
+        using the current weight attribute.
+
+        Arguemts:
+        x -- data vector for one sample or matrix containing data for several
+             samples
+        """
+        return np.dot(x, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, x):
+        """Calculate the perceptron output for given sample data using the
+        calculate_net_input method. ercetron object needs to be fitted before
+        the predict method can be used to predict class labels of new data.
+        
+        Arguments:
+        x -- data vector for one sample or matrix containing data for several
+             samples
+        """
+        return np.where(self.calculate_net_input(x) >= 0, 1, -1)
